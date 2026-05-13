296c4b33a4f812e82adf0a99eab4e49919ad658d
Huseyn Hajiyev <huseynhajiyev0@gmail.com>
2026-05-13 12:21:08 +0400

    image

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    unoptimized: true,
     remotePatterns: [
       { protocol: "https", hostname: "image.api.playstation.com" },
       { protocol: "https", hostname: "**.playstation.net" },
@@ -9,10 +10,6 @@ const nextConfig = {
       { protocol: "https", hostname: "**.supabase.in" },
       { protocol: "https", hostname: "image.tmdb.org" },
     ],
-    formats: ["image/avif", "image/webp"],
-    minimumCacheTTL: 60 * 60 * 24 * 30,
-    deviceSizes: [360, 480, 640, 750, 828, 1080, 1200, 1920],
-    imageSizes: [64, 96, 128, 200, 256, 384],
   },
 };
 
