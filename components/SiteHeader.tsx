f6ea6a7f966f2be1f925b49201cc00e4e01736e8
Huseyn Hajiyev <huseynhajiyev0@gmail.com>
2026-07-06 17:15:13 +0400

    new

--- components/SiteHeader.tsx
+++ components/SiteHeader.tsx
@@ -3,7 +3,7 @@
 import Link from "next/link";
 import Image from "next/image";
 import { usePathname } from "next/navigation";
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect } from "react";
 import {
   BriefcaseBusiness,
   ChevronDown,
